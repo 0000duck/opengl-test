c276babd88c2ef752cab420c04fab5d72d8e46e8
adamm <adamm24680@gmail.com>
2017-12-28 12:41:09 +0100

    moved shaders dir; new Mesh class

---
+++ app1/src/mesh.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adamm on 27.12.17.
+//
+
+#include "mesh.hpp"
---
+++ app1/src/mesh.hpp
@@ -0,0 +1,16 @@
+//
+// Created by adamm on 27.12.17.
+//
+
+#ifndef APP1_MESH_HPP
+#define APP1_MESH_HPP
+
+
+
+class Mesh {
+
+};
+
+
+
+#endif //APP1_MESH_HPP
