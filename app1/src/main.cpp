afa887c904c728340ee981ddfc9a226a884a8956
adamm <adamm24680@gmail.com>
2017-12-28 20:52:05 +0100

    camera

--- app1/src/main.cpp
+++ app1/src/main.cpp
@@ -6,10 +6,17 @@
 
 // Standard Headers
 
+double lastTime;
+double deltaTime;
 
 glm::mat4 model(1.0f);
 glm::mat4 view(1.0f);
 glm::mat4 projection(1.0f);
+
+glm::vec3 cameraPos(0.0f, 0.0f, -20.0f);
+glm::vec3 cameraDir(0.0f, 0.0f, 1.0f);
+
+const glm::vec3 upVector(0.0f, 1.0f, 0.0f);
 
 glm::mat4 setupMvp() {
     glm::mat4 mvp(1.0f);
@@ -18,6 +25,20 @@ glm::mat4 setupMvp() {
     mvp*= model;
     return mvp;
 
+}
+
+
+void processInput(GLFWwindow *window)
+{
+    float cameraSpeed = float(deltaTime) * 25.f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraDir;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraDir;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraDir, upVector)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraDir, upVector)) * cameraSpeed;
 }
 
 int main(int, char **) {
@@ -43,9 +64,16 @@ int main(int, char **) {
     glEnable(GL_DEPTH_TEST);
 
     Mesh mesh(PROJECT_SOURCE_DIR "/models/Cup.3DS");
+    lastTime = glfwGetTime();
     while (glfwWindowShouldClose(mWindow) == 0) {
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(mWindow, true);
+
+        double currentTime = glfwGetTime();
+        deltaTime = currentTime - lastTime;
+        lastTime = currentTime;
+
+        processInput(mWindow);
 
         glClearColor(0.7f, 0.75f, 0.8f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -53,9 +81,7 @@ int main(int, char **) {
         model = glm::rotate_slow(glm::mat4(1.0f), (float) glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
         model = glm::rotate_slow(model, (float) glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
         model = glm::rotate_slow(model, (float) glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
-        view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -6.0f));
-        view = glm::rotate_slow(view, 0.5f, glm::vec3(1.0f, 1.0f, 1.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraDir, glm::vec3(0.0f, 1.0f, 0.0f));
         glm::mat4 mvp = setupMvp();
         mesh.draw(mvp);
         glfwSwapBuffers(mWindow);
