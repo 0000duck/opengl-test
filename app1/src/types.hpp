b6ea8d5b4b7f5f02363ef9e010ee605931dad086
adamm <adamm24680@gmail.com>
2017-12-28 20:16:41 +0100

    reformat types.hpp

--- app1/src/types.hpp
+++ app1/src/types.hpp
@@ -1,5 +1,6 @@
 #ifndef APP1_TYPES_HPP
 #define APP1_TYPES_HPP
+
 #include <stdexcept>
 #include <string>
 #include <fstream>
@@ -13,11 +14,15 @@ template<GLenum bufferType, typename DataType = GLfloat>
 class BufferObject {
 public:
     BufferObject(BufferObject const &) = delete;
-    BufferObject & operator=(BufferObject const &) = delete;
+
+    BufferObject &operator=(BufferObject const &) = delete;
+
     GLuint id;
+
     BufferObject() {
         glGenBuffers(1, &id);
     }
+
     ~BufferObject() {
         glDeleteBuffers(1, &id);
     }
@@ -26,31 +31,30 @@ public:
         bind();
         glBufferData(bufferType, size * sizeof(DataType), data, GL_STATIC_DRAW);
     }
+
     void bind() {
         glBindBuffer(bufferType, id);
     }
+
     void unbind() {
         glBindBuffer(bufferType, 0);
     }
 };
 
-//template<typename VertexType>
-
-
-template <GLenum shaderType>
+template<GLenum shaderType>
 class Shader {
 private:
-    std::string readFile(const std::string& filePath) {
+    std::string readFile(const std::string &filePath) {
         std::string content;
         std::ifstream fileStream(filePath, std::ios::in);
 
-        if(!fileStream.is_open()) {
+        if (!fileStream.is_open()) {
             std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
             return "";
         }
 
         std::string line = "";
-        while(!fileStream.eof()) {
+        while (!fileStream.eof()) {
             std::getline(fileStream, line);
             content.append(line + "\n");
         }
@@ -58,70 +62,83 @@ private:
         fileStream.close();
         return content;
     }
+
 public:
-	GLuint id;
+    GLuint id;
+
     Shader(Shader const &) = delete;
-    Shader & operator=(Shader const &) = delete;
-    explicit Shader(const std::string& filename) {
-		id = glCreateShader(shaderType);
+
+    Shader &operator=(Shader const &) = delete;
+
+    explicit Shader(const std::string &filename) {
+        id = glCreateShader(shaderType);
         std::string src1 = readFile(filename);
-        const char* src = src1.c_str();
-		glShaderSource(id, 1, &src, nullptr);
-		glCompileShader(id);
-		int success;
-		glGetShaderiv(id, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			char infoLog[512];
+        const char *src = src1.c_str();
+        glShaderSource(id, 1, &src, nullptr);
+        glCompileShader(id);
+        int success;
+        glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            char infoLog[512];
             glGetShaderInfoLog(id, 512, NULL, infoLog);
             throw std::runtime_error(infoLog);
-		}
-	}
-	~Shader() {
-		glDeleteShader(id);
-	}
+        }
+    }
+
+    ~Shader() {
+        glDeleteShader(id);
+    }
 };
 
 class ShaderProgram {
 private:
-//    template<typename Arg>
-//    void doLoad(GLuint, const Arg&) {
-//    }
 
-    void doLoad(GLint loc, const glm::vec4& v) const {
+    void doLoad(GLint loc, const glm::vec4 &v) const {
         glUniform4f(loc, v.x, v.y, v.z, v.w);
     }
-    void doLoad(GLint loc, const glm::mat4& m) const {
+
+    void doLoad(GLint loc, const glm::mat4 &m) const {
         glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(m));
     }
+
+    void doLoad(GLint loc, const bool &b) const {
+        glUniform1i(loc, b ? 1 : 0);
+    }
+
 public:
     GLuint id;
 
     ShaderProgram(ShaderProgram const &) = delete;
-    ShaderProgram & operator=(ShaderProgram const &) = delete;
+
+    ShaderProgram &operator=(ShaderProgram const &) = delete;
+
     ShaderProgram() {
         id = glCreateProgram();
     }
-    template <GLenum first>
-    void linkShaderProgram(const Shader<first>& s) {
+
+    template<GLenum first>
+    void linkShaderProgram(const Shader<first> &s) {
         glAttachShader(id, s.id);
         glLinkProgram(id);
 
         int success;
-        char infoLog[512];
+        const size_t RUNTIME_ERROR_MSG_SIZE = 512;
+        char infoLog[RUNTIME_ERROR_MSG_SIZE];
         glGetProgramiv(id, GL_LINK_STATUS, &success);
-        if(!success) {
-            glGetProgramInfoLog(id, 512, NULL, infoLog);
+        if (!success) {
+            glGetProgramInfoLog(id, RUNTIME_ERROR_MSG_SIZE, NULL, infoLog);
             throw std::runtime_error(infoLog);
         }
     }
-    template <GLenum first, typename... Args>
-    void linkShaderProgram(const Shader<first>& s, const Args&... rest) {
+
+    template<GLenum first, typename... Args>
+    void linkShaderProgram(const Shader<first> &s, const Args &... rest) {
         glAttachShader(id, s.id);
         linkShaderProgram(rest...);
     }
 
     template<typename Arg>
-    void loadUniform(std::string varname, const Arg& input) const {
+    void loadUniform(const std::string& varname, const Arg &input) const {
         GLint location = glGetUniformLocation(id, varname.c_str());
         glUseProgram(id);
         doLoad(location, input);
